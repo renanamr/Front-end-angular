493ca609d046136232e2ad8b79ed962e5e054fdd
Renan A. de M. Rocha <renanrochamorais@outlook.com>
2025-01-08 10:49:57 -0300

    Adição de rotas do projeto deputados_information

---
+++ deputados_information/src/app/pages/deputado-details/deputado-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deputado-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './deputado-details.component.html',
+  styleUrl: './deputado-details.component.scss'
+})
+export class DeputadoDetailsComponent {
+
+}
---
+++ deputados_information/src/app/pages/deputados-list/deputados-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deputados-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './deputados-list.component.html',
+  styleUrl: './deputados-list.component.scss'
+})
+export class DeputadosListComponent {
+
+}
